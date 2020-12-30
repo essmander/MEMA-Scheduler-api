872ee27242c9e71ad0f28d941b30629713c50640
Fredrik Essmander <fredriklg3@gmail.com>
2020-12-30 22:41:42 +0100

    Added IdentityServer, .NETIdentity

--- Startup.cs
+++ Startup.cs
@@ -1,45 +1,49 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using IdentityServer4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace MEMA_Planning_Schedule
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment _env { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IBookingDataAccess, BookingDataAccess>();
 
+            AddIdentity(services);
+
             services.AddControllers();
+
+            services.AddRazorPages();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MEMA_Planning_Schedule", Version = "v1" });
             });
-           
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (_env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
@@ -50,12 +54,72 @@ namespace MEMA_Planning_Schedule
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseIdentityServer();
+
+           // app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapRazorPages();
             });
+        }
+
+        private void AddIdentity(IServiceCollection services)
+        {
+            services.AddDbContext<IdentityDbContext>(config =>
+                config.UseInMemoryDatabase("DevIdentity"));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+                {
+                    if (_env.IsDevelopment())
+                    {
+                        options.Password.RequireDigit = false;
+                        options.Password.RequiredLength = 4;
+                        options.Password.RequireLowercase = false;
+                        options.Password.RequireUppercase = false;
+                        options.Password.RequireNonAlphanumeric = false;
+                    }
+                })
+                .AddEntityFrameworkStores<IdentityDbContext>();
+
+            var identetyServerBuilder = services.AddIdentityServer();
+
+            identetyServerBuilder.AddAspNetIdentity<IdentityUser>();
+                
+
+            if(_env.IsDevelopment())
+            {
+                identetyServerBuilder.AddInMemoryIdentityResources(new IdentityResource[]
+                {
+                    new IdentityResources.OpenId(),
+                    new IdentityResources.Profile()
+                });
+
+                identetyServerBuilder.AddInMemoryClients(new Client[]
+                {
+                    new Client
+                    {
+                        ClientId = "web-client",
+                        AllowedGrantTypes = GrantTypes.Code,
+
+                        RedirectUris = new [] { "http://localhost:3000" },
+                        PostLogoutRedirectUris = new [] { "http://localhost:3000" },
+                        AllowedCorsOrigins = new [] { "http://localhost:3000" },
+
+                        RequirePkce = true,
+                        AllowAccessTokensViaBrowser = true,
+                        RequireConsent = false,
+                        RequireClientSecret = false,
+
+
+                    }
+                });
+
+                identetyServerBuilder.AddDeveloperSigningCredential();
+            }
         }
     }
 }
